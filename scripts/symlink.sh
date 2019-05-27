55ce74852bff0a559b11d0ac8f3422f98d2ec831
Brian Scheuermann <bscheuer@andrew.cmu.edu>
2019-05-27 12:54:38 -0400

    update symlink script to account for using git submodules

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-vim_flag=''
 opts=''
 verbose='false'
 
 while getopts 'ihv' flag; do
   case "${flag}" in
-    i) vim_flag='true' ;;
     h) opts='true' ;;
     v) verbose='true' ;;
     *) exit 1 ;;
@@ -15,7 +13,6 @@ done
 
 if [ $opts ]
 then
-  echo -i include vim symlinks
   echo -v for verbose
   echo -h to display this help
   exit 1
@@ -26,24 +23,6 @@ then
   echo clearing ~/config/old
 fi
 rm -rf ~/config/old/*
-
-
-if [ $vim_flag  ]
-then
-  if [ $verbose ]
-  then
-    echo creating vim symlinks
-  fi
-
-  mv ~/.vim/config ~/config/old
-  ln -s ~/config/vim ~/.vim/config
-
-  mv ~/.vim/UltiSnips ~/config/old
-  ln -s ~/config/vim/UltiSnips ~/.vim/UltiSnips
-
-  mv ~/.vim/after ~/config/old
-  ln -s ~/config/vim/after ~/.vim/after
-fi
 
 for file in $(ls -A ~/config/dotfiles); do
   if [ $verbose ]
