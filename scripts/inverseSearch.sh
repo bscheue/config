c99d02d94572fe8e010a31c4d7f26f0750d37e54
Brian Scheuermann <bscheuer@andrew.cmu.edu>
2019-07-24 01:13:48 -0400

    switch inverse search script to work with alacritty

--- scripts/inverseSearch.sh
+++ scripts/inverseSearch.sh
@@ -8,39 +8,20 @@ line="$2"
 [ "${line}" == "" ] && line=1
 
 exec osascript << EOF
-  tell application "iTerm"
+  tell application "Alacritty"
     # skim runs this script immediately after clicking, so delay slightly
     # to avoid accidentally including the shift in command
     # (since script is triggered on shift command click)
     delay 0.5
     activate
-    if name of current session of current window contains "vim" then
-      tell application "System Events"
+
+    tell application "System Events"
 
         keystroke "c" using control down
         keystroke ":if expand('%:p') == '$file' | $line | endif" & return
         keystroke "zz"
 
-      end tell
-    else
-      launch session "Default"
-      tell application "System Events"
-
-      keystroke "n" using command down
-      keystroke "vim $file" & return
-
-      end tell
-
-      repeat while not (name of current session of current window contains "vim")
-      end repeat
-
-      tell application "System Events"
-
-      keystroke ":$line" & return
-      keystroke "zz"
-
-      end tell
-    end if
+    end tell
 
   end tell
 EOF
