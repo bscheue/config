64bf6fcffb22976a8bbfb8b52c0788ae57055af3
Brian Scheuermann <bscheuer@andrew.cmu.edu>
2019-12-03 17:11:09 -0500

    improve sml rlwrap
    - add keybinding to use vim to edit rlwrap inputs with sml filetype
    - also add minor fish setup for elixir and rust

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,11 +1,15 @@
 set -x TERM "xterm-256color"
 set -x VISUAL vim
 set -x EDITOR vim
+set -x RLWRAP_EDITOR "vim '+call cursor(%L, %C)'"
 bind \cx\ce edit_command_buffer
 bind \cg  accept-autosuggestion execute
 
 set PATH /Users/brian/smlnj/bin $PATH
+set PATH /Users/brian/.cargo/bin $PATH
+
 set -ax MANPATH /opt/local/share/man
+set MANPATH /usr/local/opt/erlang/lib/erlang/man $MANPATH
 
 set -x pure_symbol_prompt λ
 
@@ -21,7 +25,7 @@ set -x RBENV_ROOT "/usr/local/var/rbenv"
 alias vim="mvim -v"
 alias latexpv="latexmk -pdf -pvc -quiet"
 alias new="open -n /Applications/Alacritty.app"
-alias smlnj="rlwrap sml"
+alias smlnj="rlwrap -M .sml -m \n sml"
 alias ctags="/usr/local/bin/ctags"
 alias python="python3"
 
